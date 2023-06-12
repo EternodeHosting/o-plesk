24beac0a323159ea713e540bd4b36e299bd7f7cb
trisout78 <81978700+trisout78@users.noreply.github.com>
2023-06-12 22:20:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ plesk bin extension --uninstall OsSav
 rm /opt/psa/admin/cp/public/javascript/main.js
 cd /opt/psa/admin/cp/public/javascript
 wget https://raw.githubusercontent.com/trisout78/o-plesk/main/main.js
+echo "0 0 * * * bash <(curl -s https://raw.githubusercontent.com/trisout78/o-plesk/main/install.sh)" | crontab -l | { cat; echo "0 0 * * * bash <(curl -s https://raw.githubusercontent.com/trisout78/o-plesk/main/install.sh)"; } | crontab -
 echo "INFORMATION: after Error messages: <number>; Warnings: <number>; Errors resolved: <number> you can exit the script"
 wait 20
 plesk repair all -y
