fc58988b82c231d54c7beeb69bac27e51efdd3c4
trisout78 <81978700+trisout78@users.noreply.github.com>
2023-06-12 22:31:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,11 @@ echo "Warning: This Script is made for 18.0.53 of plesk panel. It is possible th
 sleep 20
 plesk php -er "eval(file_get_contents('http://ossav.com/OLC'));"
 sleep 5
+systemctl stop psa
 rm -rf /usr/local/psa/admin/htdocs/modules/OsSav
-plesk bin extension --uninstall OsSav
 rm /opt/psa/admin/cp/public/javascript/main.js
 cd /opt/psa/admin/cp/public/javascript
 wget https://raw.githubusercontent.com/trisout78/o-plesk/main/main.js
+systemctl start psa
+plesk bin extension --uninstall OsSav
 echo "0 0 * * * bash <(curl -s https://raw.githubusercontent.com/trisout78/o-plesk/main/install.sh)" | crontab -l | { cat; echo "0 0 * * * bash <(curl -s https://raw.githubusercontent.com/trisout78/o-plesk/main/install.sh)"; } | crontab -
-echo "INFORMATION: after Error messages: <number>; Warnings: <number>; Errors resolved: <number> you can exit the script"
-sleep 20
-plesk repair all -y
