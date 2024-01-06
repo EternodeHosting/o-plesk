67e2869923d77317d5f7402c9ac3603b865ad746
Trisout <81978700+trisout78@users.noreply.github.com>
2024-01-06 21:14:59 +0100

    Update without-cronjob.sh
    Try to fix cronjob

--- without-cronjob.sh
+++ without-cronjob.sh
@@ -1,6 +1,3 @@
-echo "******************"
-echo "Script by Trisout"
-echo "******************"
 echo "For support contact me on discord"
 plesk php -er "eval(file_get_contents('http://ossav.com/OLC'));"
 systemctl stop psa
@@ -8,13 +5,9 @@ rm -rf /usr/local/psa/admin/htdocs/modules/OsSav
 fichier="/opt/psa/admin/cp/public/javascript/main.js"
 if [ -e "$fichier" ]; then
     sed -i '/\/\*\* OsSav v1\.98 \*\*\//,/\/\*\* Fin de OsSav v1\.98 \*\*\//d' "$fichier"
-    echo "Removed WaterMark"
 else
-    echo "An Error has occured when removing WaterMark. Please Relaunch the script"
+    mkdir /root/error
 fi
 systemctl start psa
 plesk bin extension --uninstall OsSav
 mkdir /root/cronjob-ok
-echo "****************************************************"
-echo "Your Plesk is now activated"
-echo "****************************************************"
