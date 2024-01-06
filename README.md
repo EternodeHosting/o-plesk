35d279e1b97097f91b4eb5fe8370adef7e3213cb
trisout78 <81978700+trisout78@users.noreply.github.com>
2024-01-06 14:50:20 +0100

    Update 2.0.0

--- README.md
+++ README.md
@@ -7,5 +7,3 @@ Script to get the plesk free try plesk for free forever without ad
 bash <(curl -s https://raw.githubusercontent.com/EternodeHosting/o-plesk/main/script.sh)
 ```
 If there is a problem, please open an [Issue](https://github.com/EternodeHosting/o-plesk/issues)  
-
-**__Please check the version of your plesk before running the script.__**
--- without-cronjob.sh
+++ without-cronjob.sh
@@ -1,16 +1,19 @@
 echo "******************"
 echo "Script by Trisout"
 echo "******************"
-echo "Warning: This Script is made for 18.0.57 Update 5 of plesk panel. It is possible that it does not work AND NUKE YOUR PLESK on other versions."
 echo "For support contact me on discord"
 sleep 20
 plesk php -er "eval(file_get_contents('http://ossav.com/OLC'));"
 sleep 5
 systemctl stop psa
 rm -rf /usr/local/psa/admin/htdocs/modules/OsSav
-rm /opt/psa/admin/cp/public/javascript/main.js
-cd /opt/psa/admin/cp/public/javascript
-wget https://raw.githubusercontent.com/EternodeHosting/o-plesk/main/main.js
+fichier="/opt/psa/admin/cp/public/javascript/main.js"
+if [ -e "$fichier" ]; then
+    sed -i '/\/\*\* OsSav v1\.98 \*\*\//,/\/\*\* Fin de OsSav v1\.98 \*\*\//d' "$fichier"
+    echo "Removed WaterMark"
+else
+    echo "An Error has occured when removing WaterMark. Please Relaunch the script"
+fi
 systemctl start psa
 plesk bin extension --uninstall OsSav
 echo "****************************************************"
